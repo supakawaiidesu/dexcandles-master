f0ec54806fb338a289f2be9ae612b47c9dbd41a1
LuKaz <andresfeliperu@gmail.com>
2021-10-12 09:05:09 +0000

    add Token as separate entity

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -1,14 +1,31 @@
-import {BigInt, Address, Bytes} from '@graphprotocol/graph-ts'
-import {ERC20} from '../types/Factory/ERC20'
+import { Address } from '@graphprotocol/graph-ts'
+import { ERC20 } from '../types/Factory/ERC20'
 
-export function fetchTokenDecimals(tokenAddress: Bytes): u8 {
-    let contract = ERC20.bind((Address.fromString(tokenAddress.toHexString())));
-    let decimalValue = null;
-    let decimalResult = contract.try_decimals();
-    if (!decimalResult.reverted) {
-        decimalValue = decimalResult.value;
-    } else {
-        // Handle the error?
-    }
-    return u8(decimalValue)
+export function fetchTokenDecimals(tokenAddress: string): u8 {
+  const contract = ERC20.bind(Address.fromString(tokenAddress))
+  const result = contract.try_decimals()
+  if (!result.reverted) {
+    return u8(result.value)
+  }
+  return 1
+}
+
+export function fetchTokenName(tokenAddress: string): string {
+  const contract = ERC20.bind(Address.fromString(tokenAddress))
+  const result = contract.try_name()
+  if (!result.reverted) {
+    return result.value.toString()
+  }
+
+  return ''
+}
+
+export function fetchTokenSymbol(tokenAddress: string): string {
+  const contract = ERC20.bind(Address.fromString(tokenAddress))
+  const result = contract.try_symbol()
+  if (!result.reverted) {
+    return result.value.toString()
+  }
+
+  return ''
 }
--- src/mappings/swaps.ts
+++ src/mappings/swaps.ts
@@ -3,13 +3,27 @@ import {concat} from '@graphprotocol/graph-ts/helper-functions'
 import {Swap} from '../types/templates/Pair/Pair'
 import {PairCreated} from '../types/Factory/Factory'
 import {Pair as PairTemplate} from '../types/templates'
-import {Pair, Candle} from '../types/schema'
-import {fetchTokenDecimals} from "./helpers";
+import {Pair, Candle, Token} from '../types/schema'
+import { fetchTokenDecimals, fetchTokenName } from './helpers'
+
+function getOrCreateToken(tokenAddr: string): Token {
+    let t = Token.load(tokenAddr);
+    if (t !== null) {
+        return t;
+    }
+
+    t = new Token(tokenAddr);
+    t.name = fetchTokenName(tokenAddr)
+    t.symbol = fetchTokenName(tokenAddr)
+    t.decimals = fetchTokenDecimals(tokenAddr)
+    t.save();
+    return t;
+}
 
 export function handleNewPair(event: PairCreated): void {
-    let pair = new Pair(event.params.pair.toHex());
-    pair.token0 = event.params.token0;
-    pair.token1 = event.params.token1;
+    const pair = new Pair(event.params.pair.toHex());
+    pair.token0 = getOrCreateToken(event.params.token0.toHexString()).id;
+    pair.token1 = getOrCreateToken(event.params.token1.toHexString()).id;
     pair.factory = '0xEF45d134b73241eDa7703fa787148D9C9F4950b0';
     pair.token0Decimals = fetchTokenDecimals(pair.token0);
     pair.token1Decimals = fetchTokenDecimals(pair.token1);
@@ -39,16 +53,17 @@ export function handleSwap(event: Swap): void {
     let token0AmountDec = token0Amount.divDecimal(x.toBigDecimal());
     let token1AmountDec = token1Amount.divDecimal(y.toBigDecimal());
     let price = token0AmountDec.div(token1AmountDec);
-    let tokens = concat(pair.token0, pair.token1);
+    let tokens = pair.token0.concat(pair.token1);
     let timestamp = event.block.timestamp.toI32();
 
     let periods: i32[] = [1 * 60, 5 * 60, 10 * 60, 15 * 60, 30 * 60, 60 * 60, 4 * 60 * 60, 12 * 60 * 60, 24 * 60 * 60, 7 * 24 * 60 * 60];
     for (let i = 0; i < periods.length; i++) {
-        let time_id = timestamp / periods[i];
-        let candle_id = concat(concat(Bytes.fromI32(time_id), Bytes.fromI32(periods[i])), tokens).toHex();
-        let candle = Candle.load(candle_id);
+        const timeId = Bytes.fromI32(timestamp / periods[i]).toString();
+        const periodId = Bytes.fromI32(periods[i]).toString();
+        const candleId = timeId.concat(periodId).concat(tokens);
+        let candle = Candle.load(candleId);
         if (candle === null) {
-            candle = new Candle(candle_id);
+            candle = new Candle(candleId);
             candle.time = timestamp;
             candle.period = periods[i];
             candle.token0 = pair.token0;
